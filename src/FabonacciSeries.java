a66607d44ad41c46a875a305103875085043592a
Akshay9522 <104826532+Akshay9522@users.noreply.github.com>
2026-03-06 01:47:05 +0530

    Update FabonacciSeries.java

--- src/FabonacciSeries.java
+++ src/FabonacciSeries.java
@@ -5,7 +5,7 @@ public class FabonacciSeries {
         int a = 0;
         int b = 1;
         System.out.print(a+","+b);
-////
+//////
         for(int i=0; i<5; i++){
             int c = a+b;
             System.out.print(","+c);
